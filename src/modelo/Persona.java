19cfe0793ce9ccb15823a2c6adf00a3a4f3e1684
jcgutierrez02 <jcgutierrez02@gmail.com>
2018-03-18 11:47:31 +0100

    se

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -4,5 +4,27 @@ public class Persona {
     
     private String nombre;
     private Integer edad;
+
+    public Persona(String nombre, Integer edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+    
     
 }
