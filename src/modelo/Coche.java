2c82a55ff2d37c2b78f8afdad30e9f991e1c9677
jcarlos <jcarlos@192.168.56.1>
2018-03-19 09:29:55 +0100

    Segundo Commit

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,4 +1,3 @@
-
 package modelo;
 
 public class Coche {
@@ -8,4 +7,30 @@ public class Coche {
     String color;
     /* Desarrollador 1 en rama Producción */
     String modelo;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
 }
